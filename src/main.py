fd3137e4e11f778f841a8ea5ac70b0feb8666ceb
amedeomorat <moratamedeo4@gmail.com>
2025-03-08 17:27:11 +0800

    fixed working code

--- src/main.py
+++ src/main.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 import argparse
 from random import randint
-import time
 
 STUDENT_ID = 2359267
 
 def main():
     parser = argparse.ArgumentParser(description="Generate a series of mathematical expressions.")
     parser.add_argument("n", help="number of mathematical expressions to write")
-    args = parser.parse_args
+    args = parser.parse_args()
     n = int(args.n)
-    start = time.localtime()
     write_in_file(str(STUDENT_ID))
     for i in range(n):
         expression = generate_expression()
-        result = eval(expression)
+        result = str(eval(expression))
         final_string = expression + "=" + result
         write_in_file(final_string)
-    stop = time.localtime()
-    time_elapsed = stop-start
 
 def generate_expression():
     MAX_NUMBER = 100
@@ -29,7 +25,7 @@ def generate_expression():
     expression = str(randint(MIN_NUMBER, MAX_NUMBER))
     operands_number = randint(MIN_OPERAND, MAX_OPERAND)
     for i in range(operands_number):
-        operand = operations[randint(0,len(operations))]
+        operand = operations[randint(0,len(operations)-1)]
         if operand == "/" and MIN_NUMBER == 0:
             number = randint(MIN_NUMBER+1,MAX_NUMBER)
         else:
@@ -38,8 +34,8 @@ def generate_expression():
     return expression
 
 def write_in_file(expression):
-    with open("result.txt", "a" ) as file:
-        file.write(f"%s\n",expression)
+    with open("..\\result.txt", "a" ) as file:
+        file.write(expression + "\n")
 
 if __name__ == "__main__":
     main()
